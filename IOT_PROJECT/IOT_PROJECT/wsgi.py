bb05e2eb86030e8034863420056c762d43f780bd
rch-goldsnaker <rch.goldsnaker@gmail.com>
2021-05-25 20:20:42 -0300

    Proyecto listo para agregar modelos

---
+++ IOT_PROJECT/IOT_PROJECT/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for IOT_PROJECT project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IOT_PROJECT.settings')
+
+application = get_wsgi_application()
