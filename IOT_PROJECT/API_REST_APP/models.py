ce878dad1fb085d6b40dace2f8d49125cce593ac
rch-goldsnaker <rch.goldsnaker@gmail.com>
2021-05-25 20:39:07 -0300

    falta cambiar la sql

--- IOT_PROJECT/API_REST_APP/models.py
+++ IOT_PROJECT/API_REST_APP/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class sensores(models.Model):
+    Tag=models.CharField(max_length=8)
+    Ubicacion=models.CharField(max_length=50)    
+    Medicion=models.CharField(max_length=50)
+    valor=models.IntegerField()
+    fecha=models.DateField()
