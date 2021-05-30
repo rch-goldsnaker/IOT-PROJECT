c32890a03f4f3ae3905ac35cf4d1bbf84fadfe49
rch-goldsnaker <rch.goldsnaker@gmail.com>
2021-05-30 14:43:58 -0300

    prueba 2

--- IOT_PROJECT/IOT_PROJECT/settings.py
+++ IOT_PROJECT/IOT_PROJECT/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&x73lu_-i--@z@&v$#1(v#%x^9t(p=zfy^@-h1j^8f6(u-eqe8'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
